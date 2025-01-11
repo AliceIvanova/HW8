078d8e1c705161676e0f20f78dafc42e49be0292
Alisa Perepelova <alice-lilo@mail.ru>
2025-01-11 21:36:46 +0300

    open

--- src/test/java/rzd/TestBase.java
+++ src/test/java/rzd/TestBase.java
@@ -2,10 +2,7 @@ package rzd;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,6 +12,10 @@ public class TestBase {
   static void setUp() {
     Configuration.browserSize = "1920x1080";
     Configuration.pageLoadStrategy = "eager";
+
+  }
+  @BeforeEach
+  void openStarPage() {
     open("https://www.rzd.ru/");
   }
   @AfterEach
